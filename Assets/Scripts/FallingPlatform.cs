180bee91249e22dd1a424aa9c581f44e7032ffc7
AntonioTheReis <antoniobalcaoreis@gmail.com>
2025-03-06 00:05:37 -0500

    Polish vanishing platform

--- Assets/Scripts/FallingPlatform.cs
+++ Assets/Scripts/FallingPlatform.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using DG.Tweening;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -24,6 +25,15 @@ public class FallingPlatform : MonoBehaviour
     private Collider2D collider2d;
     private GameObject targetPlayer;
 
+    [Header("Overlap Check")]
+    private bool overlappingWithPlayer;
+    public float collisionRadius = 0.25f;
+    public Vector2 bottomOffset, rightOffset, leftOffset;
+    private Color debugCollisionColor = Color.black;
+    public LayerMask playerLayer;
+
+    private bool wantToReturn = false;
+
     void Awake()
     {
         spriteRenderer = GetComponentInChildren<SpriteRenderer>();
@@ -34,10 +44,24 @@ public class FallingPlatform : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Shaking animation  -- Fucks up positioning
         /*if (decaying)
         {
             spriteTransform.position = new Vector2(startPos.x + Mathf.Sin(Time.time * speed) * amount, startPos.y + (Mathf.Sin(Time.time * speed) * amount));
         }*/
+
+        OverlapCheck();
+
+        if (wantToReturn && !overlappingWithPlayer)
+        {
+            Color tmp = spriteRenderer.color;
+            tmp.a = 1;
+            spriteRenderer.color = tmp;
+
+            collider2d.enabled = true;
+
+            wantToReturn = false;
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -50,22 +74,22 @@ public class FallingPlatform : MonoBehaviour
             Debug.Log("Decaying");
             if (collision.gameObject.name == "Heart Player")
             {
-                Debug.Log("Got a heart");
+                //Debug.Log("Got a heart");
                 StartCoroutine(HeartCheck());
             }
             else if (collision.gameObject.name == "Mind Player")
             {
-                Debug.Log("Got a mind");
+                //Debug.Log("Got a mind");
                 StartCoroutine(MindCheck());
             }
         }
     }
 
-    private void OnCollisionExit2D(Collision2D collision)  //Initiates the disappearance only if the player jumps off before platform disappears
+    private void OnTriggerExit2D(Collider2D collision)
     {
-        if ((collision.gameObject.name == "Heart Player") || (collision.gameObject.name == "Mind Player"))
+        if(collision.gameObject.tag == "Player")
         {
-            if (decaying) StartCoroutine(Disappear());   // !! Need to make sure it's already decaying to make it so that walljumps won't kill it, but it aint that easy! !!
+            StartCoroutine(Disappear());
         }
     }
 
@@ -86,11 +110,7 @@ public class FallingPlatform : MonoBehaviour
         DOVirtual.Float(1, 0, colorFade, DashColor);
 
         yield return new WaitForSecondsRealtime(timeToReset);
-        Color tmp = spriteRenderer.color;
-        tmp.a = 1;
-        spriteRenderer.color = tmp;
-
-        collider2d.enabled = true;
+        wantToReturn = true;
     }
 
     IEnumerator HeartCheck()
@@ -99,9 +119,9 @@ public class FallingPlatform : MonoBehaviour
         if (targetPlayer.GetComponent<Movement>().onFloor)
         {
             StartCoroutine(DecayStart());
-            Debug.Log("Was on floor");
+            //Debug.Log("Was on floor");
         }
-        else Debug.Log("Not on floor");
+        //else Debug.Log("Not on floor");
     }
 
     IEnumerator MindCheck()
@@ -110,9 +130,9 @@ public class FallingPlatform : MonoBehaviour
         if (targetPlayer.GetComponent<MindMovement>().onFloor)
         {
             StartCoroutine(DecayStart());
-            Debug.Log("Was on floor");
+            //Debug.Log("Was on floor");
         }
-        else Debug.Log("Not on floor");
+        //else Debug.Log("Not on floor");
 
     }
 
@@ -121,6 +141,32 @@ public class FallingPlatform : MonoBehaviour
         Color tmp = spriteRenderer.color;
         tmp.a = alpha;
         spriteRenderer.color = tmp;
+    }
+
+    IEnumerator LeavePlatform(GameObject collision)
+    {
+        yield return new WaitForSecondsRealtime(onFlooorCheckWait * 2);
+        if ((collision.name == "Heart Player") || (collision.name == "Mind Player"))
+        {
+            Debug.Log("Decaying: " + decaying);
+            if (decaying) StartCoroutine(Disappear());   // !! Need to make sure it's already decaying to make it so that walljumps won't kill it, but it aint that easy! !!
+        }
+    }
+
+    private void OverlapCheck()
+    {
+        overlappingWithPlayer = Physics2D.OverlapCircle((Vector2)transform.position + leftOffset, collisionRadius, playerLayer) || Physics2D.OverlapCircle((Vector2)transform.position + rightOffset, collisionRadius, playerLayer) || Physics2D.OverlapCircle((Vector2)transform.position + bottomOffset, collisionRadius, playerLayer);
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.black;
+
+        var positions = new Vector2[] { bottomOffset, rightOffset, leftOffset };
+
+        Gizmos.DrawWireSphere((Vector2)transform.position + bottomOffset, collisionRadius);
+        Gizmos.DrawWireSphere((Vector2)transform.position + rightOffset, collisionRadius);
+        Gizmos.DrawWireSphere((Vector2)transform.position + leftOffset, collisionRadius);
     }
 
 }
