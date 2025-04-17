77d3f3c4891e0bc23689cf6074da365a204180cc
Antonio Reis <107641554+AntoinTheReis@users.noreply.github.com>
2025-04-17 00:11:38 -0400

    Scene Transition stuff

--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
@@ -12,11 +14,15 @@ public class SceneChanger : MonoBehaviour
     public RawImage curtain;
     public float curtainInTime;
     public float curtainOutTime;
+    PlayerInput input = new PlayerInput();
+    private SceneChanger changer;
 
     // Start is called before the first frame update
     void Start()
     {
         FadeIn();
+        input.DeactivateInput();
+        changer = GameObject.FindGameObjectWithTag("SceneChanger").GetComponentInChildren<SceneChanger>();
     }
 
     // Update is called once per frame
@@ -54,13 +60,25 @@ public class SceneChanger : MonoBehaviour
         tmp.a = a;
         curtain.color = tmp;
 
-        if(tmp.a == 0) curtain.rectTransform.position = new Vector2(10000, 0);
+        if(tmp.a == 0)
+        {
+            curtain.rectTransform.position = new Vector2(10000, 0);
+            input.ActivateInput();
+        }
     }
 
     IEnumerator NextSceneStart()
     {
         yield return new WaitForSecondsRealtime(curtainOutTime);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            changer.NextScene();
+        }
     }
 
     IEnumerator MoveCurtainAway()
