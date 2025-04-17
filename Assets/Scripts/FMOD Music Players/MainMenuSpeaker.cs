cc2fab154ac29999bdad6f55ed462ea9b342bf97
j-Percent <chichichen1117@hotmail.com>
2025-04-17 01:43:04 -0400

    MainMenuMusic Implemented

--- Assets/Scripts/FMOD Music Players/MainMenuSpeaker.cs
+++ Assets/Scripts/FMOD Music Players/MainMenuSpeaker.cs
@@ -9,6 +9,9 @@ public class MainMenuSpeaker : MonoBehaviour
 
     public EventReference fmodEvent; // Assign FMOD Event in Inspector for Music
     private EventInstance eventInstance;
+    FMOD.Studio.PARAMETER_ID eventParameter;
+
+    public float fadeValue = 0;
 
     public EventReference fmodEvent1; // Assign FMOD Event in Inspector for Toggle
     private EventInstance eventInstance1;
@@ -16,10 +19,20 @@ public class MainMenuSpeaker : MonoBehaviour
     public EventReference fmodEvent2; // Assign FMOD Event in Inspector for Confirm
     private EventInstance eventInstance2;
 
+    public float currentValue1 = 0f;
+    public float targetValue1 = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
         eventInstance = RuntimeManager.CreateInstance(fmodEvent);
+
+        FMOD.Studio.EventDescription eventDescription;
+        eventInstance.getDescription(out eventDescription);
+        FMOD.Studio.PARAMETER_DESCRIPTION eventParameterDescription;
+        eventDescription.getParameterDescriptionByName("WhereYouAreFade", out eventParameterDescription);
+        eventParameter = eventParameterDescription.id;
+
         eventInstance1 = RuntimeManager.CreateInstance(fmodEvent1);
         eventInstance2 = RuntimeManager.CreateInstance(fmodEvent2);
 
@@ -29,7 +42,14 @@ public class MainMenuSpeaker : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // Smoothly transition towards the target value
+        currentValue1 = Mathf.Lerp(currentValue1, targetValue1, Time.deltaTime * 2);
+        eventInstance.setParameterByID(eventParameter, currentValue1);
+        if (currentValue1 >= 0.99)
+        {
+            eventInstance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+            targetValue1 = 0;
+        }
     }
 
     public void playUIToggle()
@@ -39,5 +59,10 @@ public class MainMenuSpeaker : MonoBehaviour
     public void playUIConfirm()
     {
         eventInstance2.start();
+    }
+
+    public void fadeIntroMusic()
+    {
+        targetValue1 = 1;
     }
 }
