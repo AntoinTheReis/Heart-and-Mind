7e9b8e3c4ab7492f667efd84878ef9077578eff5
inkkles <mauluzardo@gmail.com>
2025-04-30 16:11:24 -0400

    pause menu done (excluding rebinding controls)

--- Assets/PauseMenu.cs
+++ Assets/PauseMenu.cs
@@ -1,21 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Android;
+using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour
 {
-    public bool paused = true;
+    public bool paused;
     public InputAction pauseAction;
 
     public Canvas canvas;
+    public Button startingButton;
 
     private void OnEnable()
     {
+        UnityEngine.EventSystems.EventSystem.current.SetSelectedGameObject(null);
         pauseAction.Enable();
         pauseAction.performed += OnPauseActionPerformed;
         TogglePause();
     }
+
 
     private void OnPauseActionPerformed(InputAction.CallbackContext context)
     {
@@ -25,11 +31,19 @@ public class PauseMenu : MonoBehaviour
     public void TogglePause()
     {
         paused = !paused;
-        Time.timeScale = paused ? 1 : 0;
-        canvas.enabled = !paused;
+        Time.timeScale = paused ? 0 : 1;
+        canvas.enabled = paused;
 
-        
-        if(!paused) DisableInputs(); else EnableInputs();
+        if (paused)
+        {
+            UnityEngine.EventSystems.EventSystem.current.SetSelectedGameObject(startingButton.gameObject);
+            DisableInputs();
+        }
+        else
+        {
+            UnityEngine.EventSystems.EventSystem.current.SetSelectedGameObject(null);
+            EnableInputs();
+        }
     }
     
     void DisableInputs()
@@ -49,5 +63,11 @@ public class PauseMenu : MonoBehaviour
             input.EnableInput();
         }
     }
+
+    public void setSelectedButton()
+    {
+
+    }
+
     
 }
