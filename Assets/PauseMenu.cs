369aeda601c858f69a894a923608abb5d1a7f49d
Antonio Reis <107641554+AntoinTheReis@users.noreply.github.com>
2025-05-01 21:33:33 -0400

    Merge

--- Assets/PauseMenu.cs
+++ Assets/PauseMenu.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.InputSystem.Android;
+//using UnityEngine.InputSystem.Android;
 using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour
